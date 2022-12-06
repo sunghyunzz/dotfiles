f320b029f6ad8a030ad1d76a1b0e7a4d5d4349f1
Sunghyun Hwang <hwang@hey.com>
2022-12-06 16:51:46 +0900

    Decide not to use asdf (#1)

--- brew.sh
+++ brew.sh
@@ -5,14 +5,19 @@ brew tap homebrew/services
 brew tap homebrew/cask
 brew tap homebrew/cask-fonts
 
-brew install --cask iterm2 google-chrome 1password hey spotify
+brew install --cask iterm2 google-chrome 1password hey spotify obsidian tower linear-linear jetbrains-toolbox
 
 # Basic tools
 brew reinstall git
 brew install wget
 brew install tree
-brew install asdf
 brew install fzf
 brew install bat
 brew install exa
 brew install kubectl
+brew install gh
+
+# Languages
+brew install go
+brew install python
+brew install nodejs
